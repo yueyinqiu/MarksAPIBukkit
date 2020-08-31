3cf0b126d1575c7db2ff2296b7e609a6b48187c3
yueyinqiu <lujiaqi2003@126.com>
2020-08-31 21:20:13 +0800

    change the way related values goes

--- MarksAPIBukkit/MarksAPI/src/main/java/top/nololiyt/yueyinqiu/bukkitplugins/marksapi/entities/MarkRelatedValues.java
+++ MarksAPIBukkit/MarksAPI/src/main/java/top/nololiyt/yueyinqiu/bukkitplugins/marksapi/entities/MarkRelatedValues.java
@@ -1,18 +1,30 @@
 package top.nololiyt.yueyinqiu.bukkitplugins.marksapi.entities;
 
-import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permissible;
 
 /**
- * Represents some related values to get a mark. It will be possible to get per player marks by using this.
- * These values could be set differently by different authors, so you may check out the following page to know our suggestions.
- * https://github.com/yueyinqiu/MarksAPIBukkit/wiki/Suggestions-On-Related-Values
+ * Represents some related values to get a mark.
  */
 public interface MarkRelatedValues
 {
     /**
-     * Returns the related command sender.
+     * Returns the owner of the mark.
+     * With this value, different players could have different marks with a same key.
+     * It could be null and the provider should just return the static marks which don't have an owner.
+     * When it is specified, the provider should still returns the marks owned by the owner and also the static marks.
      *
-     * @return The related command sender.
+     * @return The owner.
      */
-    CommandSender getCommandSender();
+    Player getOwner();
+    
+    /**
+     * Returns permissible trying to get the mark.
+     * The provider should only return the marks the permissible have access to.
+     * Sometimes the provider can cast it to Player to get other information and decide whether the player having access to a mark.
+     * It could be null and the provider should just return all the marks.
+     *
+     * @return The permissible.
+     */
+    Permissible getPermissible();
 }
